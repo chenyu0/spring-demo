8250add1d3557000c37d2da6fd34df2f93d330ff
wzyin <wzyin@leqee.com>
2018-12-05 20:17:53 +0800

    init

---
+++ spring-job-demo/yolo-control/src/main/java/com/yolo/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.yolo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: wzyin
+ * Date: 2018/12/5
+ * Time: 20:08
+ */
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("index")
+    public String index(){
+        return "index";
+    }
+}
